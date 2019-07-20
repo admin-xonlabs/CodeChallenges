1c2c1fc82c11c183b08583d854940a40bd874e79
celestialcluster <askgowda@in.ibm.com>
2019-07-20 22:16:26 +0530

    Initial Commit

---
+++ src/SampleChallenge.java
@@ -0,0 +1,22 @@
+
+public class SampleChallenge {
+
+	public static double mPowerN(int m, int n) {
+		double result = 1;
+
+		return result;
+	}
+
+	public static void main(String[] args) {
+
+		double res1 = mPowerN(10, 2);
+		System.out.println("10 power 2 : " + res1);
+
+		double res2 = mPowerN(5, 3);
+		System.out.println("5 power 3 : " + res2);
+
+		double res3 = mPowerN(4, 4);
+		System.out.println("4 power 4 : " + res3);
+	}
+
+}
