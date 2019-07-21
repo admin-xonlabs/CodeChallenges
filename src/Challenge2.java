baec3206351aa657fcd66b95a45a3c574d514af8
Nithin Bharadwaj <nithin2812@gmail.com>
2019-07-21 11:49:09 +0530

    solution to challenge 2

--- src/Challenge2.java
+++ src/Challenge2.java
@@ -3,7 +3,7 @@ public class Challenge2 {
 
 	public static double nCr(int n, int r) {
 		double result = 0;
-
+		result= (factorial(n)/(factorial(n-r)*factorial(r)));
 		/*
 		 * Complete this method to calculate the nCr from input numbers 'n' and 'r'
 		 */
@@ -13,7 +13,7 @@ public class Challenge2 {
 
 	public static double nPr(int n, int r) {
 		double result = 0;
-
+		result= (factorial(n)/(factorial(n-r));
 		/*
 		 * Complete this method to calculate the nPr from input number 'n' and 'r'
 		 */
@@ -23,6 +23,11 @@ public class Challenge2 {
 
 	public static double factorial(int n) {
 		double result = 1;
+		int i;
+		for(i=1;i<=n;i++)
+		{
+			result=result*i;
+		}
 
 		/*
 		 * Complete this method to calculate the factorial of an input number 'n'
