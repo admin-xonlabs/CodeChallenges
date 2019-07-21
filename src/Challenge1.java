09d0e3504458033b113ffcee70518eb4cc200776
unknown <yamunaofficial22@gmail.com>
2019-07-21 11:57:36 +0530

    Challenge 1 Completed

--- src/Challenge1.java
+++ src/Challenge1.java
@@ -4,20 +4,21 @@ public class Challenge1 {
 	public static int compliment (int n)
 	{
 		int result = -1;
-		
+		bool a =Convert.ToBoolean(n);
+            	int b = Convert.ToInt32(!a);
+		return b;
 		/*
 		 * Complete this method
 		 * If the input number 'n' is 0, then the result must be 1
 		 * If the input number 'n' is 1, then the result must be 0
 		 * DO NOT USE IF-ELSE, SWITCH-CASE, MODULUS, LOOPING, or any other conditional or control statements
 		 */
-		
-		
-		
-		return result;
 	}
 	
 	public static void main(String arg[]) {
+		int a = compliment(0);
+                Console.Write(a);
+                Console.ReadLine();
 
 	}
 }
