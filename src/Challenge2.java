d082c4e108526464c14bb2e681448d303fc142fa
yamunaofficial22 <53134574+yamunaofficial22@users.noreply.github.com>
2019-07-21 12:25:37 +0530

    Add files via upload

--- src/Challenge2.java
+++ src/Challenge2.java
@@ -1,3 +1,10 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
 
 public class Challenge2 {
 
@@ -7,42 +14,38 @@ public class Challenge2 {
 		/*
 		 * Complete this method to calculate the nCr from input numbers 'n' and 'r'
 		 */
-
-		return result;
+		return fact(n) / (fact(r) * fact(n - r)); 
+		
 	}
 
 	public static double nPr(int n, int r) {
-		double result = 0;
-
+	 
 		/*
 		 * Complete this method to calculate the nPr from input number 'n' and 'r'
 		 */
-
-		return result;
+				double per=fact(n)/fact(n-r);	
+				return per;
 	}
 
-	public static double factorial(int n) {
-		double result = 1;
-
-		/*
-		 * Complete this method to calculate the factorial of an input number 'n'
-		 */
-
-		return result;
+	public static double fact(int n) {
+		int res = 1; 
+    		for (int i = 2; i <= n; i++) 
+        		res = res * i; 
+    		return res; 
 	}
 
 	public static void main(String arg[]) {
 
-		double res1 = factorial(2);
+		double res1 = fact(2);
 		System.out.println("Factorial of 2 is : " + res1);
 
-		double res2 = factorial(4);
+		double res2 = fact(4);
 		System.out.println("Factorial of 140 is : " + res2);
 
-		double res3 = factorial(6);
+		double res3 = fact(6);
 		System.out.println("Factorial of 6 is : " + res3);
 
-		double res4 = factorial(8);
+		double res4 = fact(8);
 		System.out.println("Factorial of 8 is : " + res4);
 
 		double res5 = nCr(5, 2);
