a0304393dc3adf2fe617310b52ece337dd33ade8
Nithin Bharadwaj <nithin2812@gmail.com>
2019-07-22 22:40:36 +0530

    [vision of us] solution to challenge 3

--- src/Challenge3.java
+++ src/Challenge3.java
@@ -1,28 +1,56 @@
 
 public class Challenge3 {
 
-	public static int findMaxOccurrence(int[] numbers) {
-		int element = -1;
-
+	public static int findMaxOccurrence(int arr[],int n)
+	{
+          	// find the max frequency using linear 
+        	// traversal 
+        	int max_count = 1, res = arr[0]; 
+        	int curr_count = 1; 
+          
+        	for (int i = 1; i < n; i++) 
+        	{ 
+            		if (arr[i] == arr[i - 1]) 
+                		curr_count++; 
+            		else 
+            		{ 
+                		if (curr_count > max_count) 
+                		{ 
+                    			max_count = curr_count; 
+                    			res = arr[i - 1]; 
+                		} 
+                		curr_count = 1; 
+            		} 
+        	} 
+		// If last element is most frequent 
+        	if (curr_count > max_count) 
+        	{ 
+            		max_count = curr_count; 
+            		res = arr[n - 1]; 
+        	} 
 		/*
 		 * Parse the input array and find out the element with most number of
 		 * occurrence.
 		 */
 
-		return element;
+		return res;
 	}
 
-	public static void main(String arg[]) {
+	public static void main(String arg[]) 
+	{
 		int arr1[] = { 10, 20, 20, 40, 40, 40, 12, 25 };
-		int res1 = findMaxOccurrence(arr1);
+		int n1 = arr1.length;
+		int res1 = findMaxOccurrence(arr1,n1);
 		System.out.println("Most number of occurrences in the first array is : " + res1);
 
 		int arr2[] = { 1, 1, 1, 2, 2, 2 };
-		int res2 = findMaxOccurrence(arr2);
+		int n2 = arr2.length;
+		int res2 = findMaxOccurrence(arr2,n2);
 		System.out.println("Most number of occurrences in the second array is : " + res2);
 
 		int arr3[] = { 1, 2, 3, 3, 5, 4 };
-		int res3 = findMaxOccurrence(arr3);
+		int n3 = arr3.length;
+		int res3 = findMaxOccurrence(arr3,n3);
 		System.out.println("Most number of occurrences in the third array is : " + res3);
 
 	}
