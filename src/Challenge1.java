2cabca9e7e26444aa94b7d910a6aaace469bbe61
Prajval H L <prajvalhl@gmail.com>
2019-07-23 11:23:47 +0530

    Updated Challenge 1

--- src/Challenge1.java
+++ src/Challenge1.java
@@ -1,24 +1,21 @@
-
 public class Challenge1 {
 
 	public static int compliment (int n)
 	{
-		// this code only works in c# 
-		bool a =Convert.ToBoolean(n);
-            	int b = Convert.ToInt32(!a);
-		return b;
+		int result = -1;
 		/*
 		 * Complete this method
 		 * If the input number 'n' is 0, then the result must be 1
 		 * If the input number 'n' is 1, then the result must be 0
 		 * DO NOT USE IF-ELSE, SWITCH-CASE, MODULUS, LOOPING, or any other conditional or control statements
 		 */
+		result = n>0? 0 : 1;
+		return result;
 	}
 	
-	public static void main(String arg[]) {
-		int a = compliment(0);
-                Console.Write(a);
-                Console.ReadLine();
-
+	public static void main(String arg[]) 
+	{
+		int a = compliment(1);
+		System.out.println(a);
 	}
 }
