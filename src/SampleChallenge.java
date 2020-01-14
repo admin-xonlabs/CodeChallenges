4639baf1e1a57c58110e12152b4889895278ee26
Anusha AG <anushashrike1999@gmail.com>
2020-01-14 17:09:40 +0530

    solved the sample challenge

--- src/SampleChallenge.java
+++ src/SampleChallenge.java
@@ -4,6 +4,11 @@ public class SampleChallenge {
 	public static double mPowerN(int m, int n) {
 		double result = 1;
 
+		for(int i=1;i<=n;i++)
+		{
+			result=result*m;
+		}
+
 		/*
 		Complete this method to return the value of mPowerN. 
 		Note: Do not use Math.pow() method.
