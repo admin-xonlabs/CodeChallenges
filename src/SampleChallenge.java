7025018c1a0010fd69165a0bce7187ad20af1c94
vishwanath16 <vishu.vishwanath16@gmail.com>
2020-01-14 17:09:53 +0530

    solved it!!

--- src/SampleChallenge.java
+++ src/SampleChallenge.java
@@ -1,19 +1,22 @@
 
-public class SampleChallenge {
-
-	public static double mPowerN(int m, int n) {
+public class SampleChallenge 
+{
+	public static double mPowerN(int m, int n) 
+	{
 		double result = 1;
-
 		/*
-		Complete this method to return the value of mPowerN. 
-		Note: Do not use Math.pow() method.
-		*/
-		
+		 * Complete this method to return the value of mPowerN. Note: Do not use
+		 * Math.pow() method.
+		 */
+		for(int i = 0; i < n; i++)
+		{
+			result *= m;
+		}
 		return result;
 	}
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args)
+	{
 		double res1 = mPowerN(10, 2);
 		System.out.println("10 power 2 : " + res1);
 
@@ -23,5 +26,4 @@ public class SampleChallenge {
 		double res3 = mPowerN(4, 4);
 		System.out.println("4 power 4 : " + res3);
 	}
-
 }
