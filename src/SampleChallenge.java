b7fddaec675be1a9e7c2299164be114d1ff980c7
thanush <thanushr405@gmail.com>
2020-01-15 23:18:35 +0530

    trialtrial

--- src/SampleChallenge.java
+++ src/SampleChallenge.java
@@ -3,7 +3,7 @@ public class SampleChallenge {
 
 	public static double mPowerN(int m, int n) {
 		double result = 1;
-
+shivam
 		/*
 		Complete this method to return the value of mPowerN. 
 		Note: Do not use Math.pow() method.
