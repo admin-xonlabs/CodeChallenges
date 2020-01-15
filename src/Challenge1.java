c33d95d995fc8fff9626952d1429b708f24fbf92
darshan <darshanvd13197@gmail.com>
2020-01-15 16:06:55 +0530

    Challenge 1 is Completed

--- src/Challenge1.java
+++ src/Challenge1.java
@@ -3,18 +3,9 @@ public class Challenge1 {
 
 	public static int compliment (int n)
 	{
-		int result = -1;
-		
-		/*
-		 * Complete this method
-		 * If the input number 'n' is 0, then the result must be 1
-		 * If the input number 'n' is 1, then the result must be 0
-		 * DO NOT USE IF-ELSE, SWITCH-CASE, MODULUS, LOOPING, or any other conditional or control statements
-		 */
-		
-		
-		
-		return result;
+	
+       			 return (n == '0') ? '1' : '0'; 
+    		
 	}
 	
 	public static void main(String arg[]) {
