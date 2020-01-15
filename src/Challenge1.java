c686afe1ca666626be0b2ed7b88f3789be723d1f
darshan <darshanvd13197@gmail.com>
2020-01-15 16:23:37 +0530

    cjallenge 1

--- src/Challenge1.java
+++ src/Challenge1.java
@@ -2,7 +2,9 @@ public class Challenge1 {
 
 	public static int compliment (int n)
 	{
-       			 return (n == '0') ? '1' : '0'; 
+       			 
+		return (n == '0') ? '1' : '0'; 
+	
 	}
 	
 	public static void main(String arg[]) {
