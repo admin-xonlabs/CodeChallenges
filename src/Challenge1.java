593522cba8d378cb1d29a52d626ccc4761851c25
Chinmaya SH <chinmaya.siddarth@gmail.com>
2020-01-15 22:26:51 +0530

    done with changes

--- src/Challenge1.java
+++ src/Challenge1.java
@@ -1,23 +1,20 @@
-
-public class Challenge1 {
-
-	public static int compliment (int n)
+public static int compliment (int n)
 	{
 		int result = -1;
-		
+
+		n=0;
+		//taking a as input
+		result=1-n;	
+
 		/*
 		 * Complete this method
 		 * If the input number 'n' is 0, then the result must be 1
-		 * If the input number 'n' is 1, then the result must be 0
-		 * DO NOT USE IF-ELSE, SWITCH-CASE, MODULUS, LOOPING, or any other conditional or control statements
-		 */
-		
-		
-		
-		return result;
+@@ -18,6 +21,7 @@ public static int compliment (int n)
 	}
-	
+
 	public static void main(String arg[]) {
 
+int result=0;
+		System.out.println("compliment of "+result+" is "+compliment(result));
 	}
 }
